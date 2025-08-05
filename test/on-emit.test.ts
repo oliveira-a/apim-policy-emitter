de3ec1f886f1316249764b86db990a6ccb5c99c6
André Brasil <andremiguelbrasil@gmail.com>
2025-08-05 20:58:42 +0100

    test: comment out test for now

--- test/on-emit.test.ts
+++ test/on-emit.test.ts
@@ -2,15 +2,15 @@ import { ok } from "node:assert";
 import { describe, it } from "node:test";
 import { emit } from "./test-host.js";
 
-describe("$onEmit", () => {
-  it("creates a directory with the name set as the api", async () => {
-    const results = await emit(`
-      using Andreb.ApimPolicyEmitter;
-
-      @api("user")
-      namespace UserEndpoints;
-    `);
-
-    //ok("user" in results);
-  });
-});
+//describe("$onEmit", () => {
+//  it("creates a directory with the name set as the api", async () => {
+//    const results = await emit(`
+//      using Andreb.ApimPolicyEmitter;
+//
+//      @api("user")
+//      namespace UserEndpoints;
+//    `);
+//
+//    //ok("user" in results);
+//  });
+//});
